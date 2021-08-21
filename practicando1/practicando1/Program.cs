4196d510b22c7c9cd89263398b9b1b217dcc1fb1
Eduardo Williams <eduardowilliams300599@gmail.com>
2021-08-20 22:03:56 -0600

    1.0 - solo he echo una clase la cual le digitan el nombre, edad y direccion

---
+++ practicando1/practicando1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace practicando1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------BIENVENIDO SEÑOR----------");
+
+            Digitar digitar = new Digitar();
+
+            digitar.setInformacion("williams", 22, "teodoro martinez");
+            Console.WriteLine($"Mi nombre es {digitar.getNombre()} y tengo {digitar.getEdad()} años y vivo en el barrio {digitar.getDireccion()}\n");
+        }
+    }
+
+    class Digitar
+    {
+        private string nombre;
+        byte edad;
+        string direccion;
+
+        public Digitar()
+        {
+            nombre = "eduardo";
+            edad = 22;
+        }
+
+        public void setInformacion(string nombre, byte edad, string direccion)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.direccion = direccion;
+        }
+
+        public string getNombre() => nombre;
+        public byte getEdad() => edad;
+        public string getDireccion() => direccion;
+    }
+}
